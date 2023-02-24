9f5643bbbcab0d6216191fc39e4a3c9ce4473ee9
Mick L <90425599+Micklitodev@users.noreply.github.com>
2023-02-23 23:46:27 -0500

    0.1.0
    added in basic application structure that sucessfully runs prompts gathering user input then submits user input and writes readme.md file with all criteria input to the terminal.

--- README.md
+++ README.md
@@ -1,2 +1,82 @@
-# Gen-o-readme
-Node.js - app to dynamically generate a read me file by answering terminal prompts.
+
+    # test
+
+
+    ## Table of contents
+
+    1. [ Link ](#link)
+    2. [ ScreenShot ](#ss)
+    3. [ Description ](#descr)
+    4. [ Installation ](#install)
+    5. [ Usage ](#usage)
+    6. [ License ](#license)
+    7. [ Contributions ](#contributions)
+    8. [ Test ](#test)
+    9. [ Questions ](#question)
+
+
+
+    <a name="link"></a>
+    ## Link
+    
+    Click [box] to go to the live link.
+    
+
+    <a name="ss"></a>
+    ## ScreenShot
+    
+    ![image](snap)
+
+
+    
+    <a name="descr"></a>
+    ## Description
+   
+    - crackle
+
+
+
+
+    <a name="install"></a>
+    ## Installation
+    
+    - pop
+
+
+
+
+    <a name="usage"></a>
+    ## Usage
+    
+    - yaga
+
+    
+
+
+    <a name="license"></a>
+    ## License
+
+    applesauce
+   
+
+
+    <a name="contributions"></a>
+    ## Contributions
+
+    
+    sandwhich
+
+
+    
+    
+    #Tests
+    
+    cupcake
+
+
+
+    #Questions
+    
+    snaks
+
+    
--- index.js
+++ index.js
@@ -1,13 +1,161 @@
-// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-// TODO: Create an array of questions for user input
-const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const questions = ['what is the name of your project?', 
+'what is the deployed link to your project?', 
+'What is the image path your using for deployed screenshot',
+'Explain your project.',
+'Who created this project?',
+'what is their github profile url?'];
 
-// TODO: Create a function to initialize app
-function init() {}
 
-// Function call to initialize app
-init();
+
+inquirer 
+    .prompt([
+        {
+            type: "input",
+            message: 'what is the name of your project?',
+            name: "title",
+        },
+        {
+            type: "input",
+            message: 'what is the deployed link URL to your project?',
+            name: "link",
+        },
+        {
+            type: "input",
+            message: 'what is the name of your project?',
+            name: "img",
+        },
+        {
+            type: "input",
+            message: 'what is the deployed link URL to your project?',
+            name: "descr",
+        },
+        {
+            type: "input",
+            message: 'what is the name of your project?',
+            name: "install",
+        },
+        {
+            type: "input",
+            message: 'what is the deployed link URL to your project?',
+            name: "usage",
+        },
+        {
+            type: "input",
+            message: 'what is the name of your project?',
+            name: "license",
+        },
+        {
+            type: "input",
+            message: 'what is the deployed link URL to your project?',
+            name: "contributor",
+        },
+        {
+            type: "input",
+            message: 'what is the name of your project?',
+            name: "tests",
+        },
+        {
+            type: "input",
+            message: 'what is the name of your project?',
+            name: "github",
+        }
+    ]).then((data) => {
+        const preloadGen = genReadme(data)
+        
+        fs.writeFile('README.MD', preloadGen, (err) => {
+            err ? console.log(err) : console.log('sucess')
+        })
+    })
+
+
+
+function genReadme({title, link, img, descr, install, usage, license, contributor, tests, github}) {
+  return (
+    `
+    # ${title}
+
+
+    ## Table of contents
+
+    1. [ Link ](#link)
+    2. [ ScreenShot ](#ss)
+    3. [ Description ](#descr)
+    4. [ Installation ](#install)
+    5. [ Usage ](#usage)
+    6. [ License ](#license)
+    7. [ Contributions ](#contributions)
+    8. [ Test ](#test)
+    9. [ Questions ](#question)
+
+
+
+    <a name="link"></a>
+    ## Link
+    
+    Click [${link}] to go to the live link.
+    
+
+    <a name="ss"></a>
+    ## ScreenShot
+    
+    ![image](${img})
+
+
+    
+    <a name="descr"></a>
+    ## Description
+   
+    - ${descr}
+
+
+
+
+    <a name="install"></a>
+    ## Installation
+    
+    - ${install}
+
+
+
+
+    <a name="usage"></a>
+    ## Usage
+    
+    - ${usage}
+
+    
+
+
+    <a name="license"></a>
+    ## License
+
+    ${license}
+   
+
+
+    <a name="contributions"></a>
+    ## Contributions
+
+    
+    ${contributor}
+
+
+    
+    
+    #Tests
+    
+    ${tests}
+
+
+
+    #Questions
+    
+    ${github}
+
+    `);
+
+};
