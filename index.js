7b76fbfc3d861bc55c83d62a78885f6d65ee6c7d
Mick L <90425599+Micklitodev@users.noreply.github.com>
2023-02-26 01:51:23 -0500

    0.3.0
    fixed the formatting of the way the readme was rendered, changed an input response to selection response for licenseing added in a question for the email response. fixed bug where link was rendering as the link instead of a click here option under ## link

--- index.js
+++ index.js
@@ -9,76 +9,83 @@ function genReadme({
   descr,
   install,
   usage,
-  license,
   contributor,
   tests,
   github,
+  email, 
+  license
 }) {
   return `
   # ${title}
 
+
+  ## Description
+  
+  ${descr}
+
+
+
   ## Table of contents
   
-  1. [ Link ](#link)
-  2. [ ScreenShot ](#ss)
-  3. [ Description ](#descr)
-  4. [ Installation ](#install)
-  5. [ Usage ](#usage)
-  6. [ License ](#license)
-  7. [ Contributions ](#contributions)
-  8. [ Test ](#test)
-  9. [ Questions ](#question)
+  - [ Link ](#link)
+  - [ ScreenShot ](#ScreenShot)
+  - [ Installation ](#Installation)
+  - [ Usage ](#Usage)
+  - [ Contributions ](#contributions)
+  - [ Tests ](#tests)
+  - [ Questions ](#questions)
+  - [ License ](#license)
   
-  <a name="link"></a>
-  
+
   ## Link
   
-  Click [${link}] to go to the live link.
+  [Click Here](${link}) to go to the live link.
   
-  <a name="ss"></a>
-  
+
+
   ## ScreenShot
   
   ![image](${img})
   
-  <a name="descr"></a>
-  
-  ## Description
-  
-  ${descr}
-  
-  <a name="install"></a>
+
   
   ## Installation
   
   ${install}
-  
-  <a name="usage"></a>
+
+
   
   ## Usage
   
   ${usage}
   
-  <a name="license"></a>
-  ${licenses(license)}
-  
-  <a name="contributions"></a>
   
   ## Contributions
   
+  How to contribute?: 
   ${contributor}
   
-  <a name="test"></a>
+
   
   ## Tests
   
   ${tests}
   
-  <a name="question"></a>
+
   
   ## Questions
   
   ${github}
+
+  ${email}
+
+  When contact me please be mindful of timezones, Please check my profile for my
+  current time information. Also, if it pertains to a bug, Please, use a clear,
+  legible, instructions on how to recreate this bug as well as any videos or 
+  screenshots will be appriciated. Please also make the subject the specified project
+  name with a short description of what the contact pertains to. Thank you! 
+
+  ${licenses(license)}
   
   `;
 }
@@ -117,12 +124,7 @@ inquirer
     },
     {
       type: "input",
-      message: "what is the name of license being used",
-      name: "license",
-    },
-    {
-      type: "input",
-      message: "Who are the contributors?",
+      message: "How do users contribute?",
       name: "contributor",
     },
     {
@@ -134,9 +136,21 @@ inquirer
     {
       type: "input",
       message:
-        "what github do they contact if they have questions regarding this repo?",
+        "Who do they contact if they have questions regarding this repo?",
       name: "github",
     },
+    {
+      type: "input",
+      message:
+        "What is the best email for people to contact you regarding quesitons?",
+      name: "email",
+    },
+    {
+      type: "list",
+      message: "what is the name of license being used",
+      name: "license",
+      choices: ['MIT', 'GPL', 'BSD', 'ASF', 'CDDL', 'EPL']
+    }
   ])
   .then((data) => {
     const preloadGen = genReadme(data);
